ef69cea720c4e546cd4fdf9260c312486a28cd6c
flobby41 <florian.cheheb@gmail.com>
2025-02-27 06:28:07 +0100

    ProductName add to Cart model

--- backend/models/Cart.js
+++ backend/models/Cart.js
@@ -6,6 +6,7 @@ const CartSchema = new mongoose.Schema({
   items: [
     {
       productId: { type: mongoose.Schema.Types.ObjectId, ref: 'Product', required: true },
+      productName: { type: String, required: true },
       quantity: { type: Number, required: true, default: 1 },
       price: { type: Number, required: true }, // Sauvegarde du prix au moment de l'ajout
     }
