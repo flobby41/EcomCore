d6653ef415b5db4237ffef99b2015b7909d21dba
flobby41 <florian.cheheb@gmail.com>
2025-02-28 09:44:26 +0100

    UI enhancing index page alignment

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,4 +1,6 @@
-import Link from 'next/link';
+import { useEffect, useState } from "react";
+import Link from "next/link";
+import Layout from "@/components/Layout";
 
 export default function Home() {
   return (
